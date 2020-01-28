280b5dbdd065ab87ab4750652384d94198cdfea0
Diego Sanchez <dgsanchez@synopsis.ws>
2020-01-28 18:36:06 -0500

    Update SearchParams - 28/01/2020

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1> Adopt me</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese"></Pet>
-      <Pet name="Pepper" animal="bird" breed="Cockatiel"></Pet>
-      <Pet name="Pepper" animal="cat" breed="Mix"></Pet>
+        <SearchParams />
     </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
